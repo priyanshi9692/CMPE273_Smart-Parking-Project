479d9f2598a9001a5f0f15874dcd197d379cef36
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2019-05-07 12:15:58 -0700

    Updated details]

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ const app = express();
 
 //Imports to route
  const {getHomePage} = require('./routes/index');
+ const {getPlanPage} = require('./routes/index');
  const {getPaymentPage} = require('./routes/index');
  const {getGoogleMap} = require('./routes/index');
 const {postPaymentInfo}= require('./routes/index');
+const {getSpots} = require('./routes/index');
+const {getSpotsStatus} = require('./routes/index');
+const {getEmptySpot} = require('./routes/index');
+const {getVehicle} = require('./routes/index');
+const {leaveSpot} = require('./routes/index');
+const {parkSpot} = require('./routes/index');
+const {getAllSpots} = require('./routes/index');
+const {emailEmptySpot}=require('./routes/index');
 
 /* Set up values */
 const port = 5000;
@@ -28,11 +37,19 @@ app.use(fileUpload());
 // routes for our app
 app.get('/', getHomePage);
 app.get('/payment', getPaymentPage);
+app.get('/plan', getPlanPage);
 app.get('/GoogleMap', getGoogleMap);
 app.post('/paymentInfo', postPaymentInfo);
-
-
-
+app.get('/spots', getSpots);
+app.get('/vehicles',getVehicle);
+app.get('/getallspots',getAllSpots);
+//Action Routes
+app.get('/spots-status', getSpotsStatus);
+app.post('/paymentInfo', postPaymentInfo);
+app.get('/emptyspot/:type',getEmptySpot);
+app.get('/leave/:type',leaveSpot);
+app.get('/park/:type',parkSpot);
+app.post('/email',emailEmptySpot);
 
 // set the app to listen on the port
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -35,15 +35,261 @@ module.exports = {
     res.render('home.ejs');
   },
 
+  getPlanPage: (req, res) => {
+
+    res.render('plan.ejs');
+  },
+
   getPaymentPage: (req, res) => {
     console.log(req.params.id);
     res.render('payment.ejs');
   },
   getGoogleMap:(req,res)=>{
-    console.log('GoogleMap');
     res.render('login.ejs');
   },
+  getSpots:(req,res)=>{
+    res.render('spots.ejs');
+  },
 
+  getSpotsStatus:(req,res)=>{
+
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("parking_lot");
+      //var myobj = paymentInfo;
+     
+      dbo.collection("parkingSlots").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        //console.log(result);
+        var data =[];
+        for(var i=0;i<result.length;i++){
+          var allSlots={};
+          allSlots.status=result[i].isSlotFull;
+          allSlots.slot = result[i].slot_num;
+          allSlots.slot_type = result[i].slot_type;
+          data.push(allSlots);
+        }
+        //Mail code
+        // mailOptions.to = paymentInfo.email + "; priyanshi.jajoo@sjsu.edu";
+        // mailOptions.subject = "Congratulations!!!Registered as Customer";
+        // mailOptions.html = "Hi <b>" + paymentInfo.firstname + "</b>, " + "<br /> <br /> You are registered as our customer . <br/><br /> Regards, <br /> CMPE-272";
+        // transporter.sendMail(mailOptions, function (error, info) {
+        //   if (error) {
+        //     console.log(error);
+        //   } else {
+        //     console.log('Sending success email ' + info.response);
+        //   }
+        // });
+
+        db.close();
+        console.log(data);
+        res.send(data);
+        
+        return;
+      });
+    });
+    
+  },
+  getVehicle:(req,res)=>{
+    res.render('cardemo.ejs');
+  },
+
+  getEmptySpot:(req,res)=>{
+    var type = req.params.type;
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("parking_lot");
+      //var myobj = paymentInfo;
+      var query = { slot_type: type,isSlotFull:false };
+      dbo.collection("parkingSlots").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        var data =[];
+        if(result == [] || result == undefined){
+          db.close();
+          return res.send("Sorry Parking Full");
+        } else {
+          db.close();
+        return res.send("Please go to Slot no: <b>" + result[0].slot_num + "</b> on " + result[0].block_code + " on floor " + result[0].floor_no);
+        }
+      
+       
+        //
+        
+      });
+    });
+  },
+
+
+emailEmptySpot: (req,res)=>{
+  var customerInfo={};
+  console.log(req.body);
+  customerInfo.email = req.body.email;
+  customerInfo.vehiclenumber=req.body.vehicle;
+  customerInfo.type=req.body.type;
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("parking_lot");
+    var query = { slot_type: customerInfo.type ,isSlotFull:false };
+    dbo.collection("parkingSlots").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      // var data =[];
+      if(result == [] || result == undefined){
+       
+        mailOptions.to = customerInfo.email + "; priyanshi.jajoo@sjsu.edu";
+        mailOptions.subject = "Registered!!!";
+        mailOptions.html = "Hi <b>" + "Sorry Parking Full" + "</b>, " + "<br /> <br /> Regards, <br /> CMPE-273";
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Sending success email ' + info.response);
+          }
+          db.close();
+        });
+
+        return res.send('home.ejs');
+      } 
+      else {
+        
+        mailOptions.to = customerInfo.email + "; priyanshi.jajoo@sjsu.edu";
+        mailOptions.subject = "Registered!!!";
+        mailOptions.html = "Hi <b>" + "Please go to Slot no: <b>" + result[0].slot_num + "</b> on " + result[0].block_code + " on floor " + result[0].floor_no + "</b>, " + "<br /> <br /> You are registered as our customer . <br/><br /> Regards, <br /> CMPE-273";
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Sending success email ' + info.response);
+          }
+          db.close();
+        });
+
+      return res.send('home.ejs');
+      }
+      
+    });
+  });
+  //console.log(JSON.stringify(paymentInfo));
+
+      
+      
+  
+  // MongoClient.connect(url, function (err, db) {
+  //   if (err) throw err;
+  //   var dbo = db.db("customer");
+  //   var myobj =customerInfo ;
+  //   dbo.collection("customerDetails").insertOne(myobj, function (err, res) {
+  //     if (err) throw err;
+  //     // console.log(result);
+  //     mailOptions.to = customerInfo.email + "; priyanshi.jajoo@sjsu.edu";
+  //     mailOptions.subject = "Registered!!!";
+  //     mailOptions.html = "Hi <b>" + customerInfo.fullname + "</b>, " + "<br /> <br /> You are registered as our customer . <br/><br /> Regards, <br /> CMPE-272";
+  //     transporter.sendMail(mailOptions, function (error, info) {
+  //       if (error) {
+  //         console.log(error);
+  //       } else {
+  //         console.log('Sending success email ' + info.response);
+  //       }
+  //     });
+  //     // // var data =[];
+  //     // if(result == [] || result == undefined){
+  //     //   db.close();
+  //     //   return res.render("home.ejs");
+  //     // } else {
+  //     //   db.close();
+  //     // return res.send("Please go to Slot no: <b>" + result[0].slot_num + "</b> on " + result[0].block_code + " on floor " + result[0].floor_no);
+  //     // }
+  //   });
+  // });
+    
+},
+  getAllSpots: (req,res)=>{
+
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("parking_lot");
+      //var myobj = paymentInfo;
+      var query = { isSlotFull:false };
+      dbo.collection("parkingSlots").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        //console.log(result);
+        var data={};
+        data.carcount=0;
+        data.bikecount=0;
+        data.truckcount=0;
+        for(var i=0;i<result.length;i++){
+          if(result[i].slot_type == 'bike'){
+            data.bikecount++;
+        } else if(result[i].slot_type == 'car'){
+          data.carcount++;
+        } else if(result[i].slot_type == 'truck'){
+          data.truckcount++;
+        }
+        }
+        //Mail code
+        // mailOptions.to = paymentInfo.email + "; priyanshi.jajoo@sjsu.edu";
+        // mailOptions.subject = "Congratulations!!!Registered as Customer";
+        // mailOptions.html = "Hi <b>" + paymentInfo.firstname + "</b>, " + "<br /> <br /> You are registered as our customer . <br/><br /> Regards, <br /> CMPE-272";
+        // transporter.sendMail(mailOptions, function (error, info) {
+        //   if (error) {
+        //     console.log(error);
+        //   } else {
+        //     console.log('Sending success email ' + info.response);
+        //   }
+        // });
+
+        db.close();
+        console.log(data);
+        res.send(data);
+        
+        return;
+      });
+    });
+    
+  },
+  parkSpot:(req,res)=>{
+    var type = req.params.type;
+    console.log(type);
+    var myquery = { slot_num: type };
+    var newvalues = { $set: { isSlotFull:true} };
+    console.log(newvalues);
+    //var myquery = { slot_num: type };
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("parking_lot");
+      //var myobj = paymentInfo;
+      var query = { slot_type: type,isSlotFull:false };
+      dbo.collection("parkingSlots").updateOne(myquery, newvalues, function(err, result) {
+        if (err) throw err;
+        console.timeLog(result);
+        console.log("1 document updated");
+        db.close();
+       
+      });
+      return res.send("Successfully Parked");
+    });
+  },
+  leaveSpot:(req,res)=>{
+    var type = req.params.type;
+    console.log(type);
+    var myquery = { slot_num: type };
+    var newvalues = { $set: { isSlotFull:false} };
+    console.log(newvalues);
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("parking_lot");
+      //var myobj = paymentInfo;
+      //var query = { slot_type: type,isSlotFull:false };
+      dbo.collection("parkingSlots").updateOne(myquery, newvalues, function(err, result) {
+        if (err) throw err;
+        console.log("1 document updated");
+        db.close();
+        return res.send("Succcess");
+      });
+      
+    });
+  },
 
   //refer
   postPaymentInfo: (req, res) => {
